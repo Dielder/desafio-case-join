cd099cb1c8e09aa9f408967510f78faa11543ff0
Dielder <dieldergabrieleal@hotmail.com>
2025-01-15 15:55:47 -0300

    service do produto criado

---
+++ produtos/src/main/java/com/dieldev/produtos/service/ProdutoService.java
@@ -0,0 +1,33 @@
+package com.dieldev.produtos.service;
+
+import com.dieldev.produtos.model.Produto;
+import com.dieldev.produtos.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public Produto salvar(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public List<Produto> listarTodos() {
+        return produtoRepository.findAll();
+    }
+
+    public Optional<Produto> buscarPorId(UUID id) {
+        return produtoRepository.findById(id);
+    }
+
+    public void deletar(UUID id) {
+        produtoRepository.deleteById(id);
+    }
+}
+
